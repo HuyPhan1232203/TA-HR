053b52e996fb917f5f6f13146298c0b9c5608ac9
HuyPhan1232203 <154007180+HuyPhan1232203@users.noreply.github.com>
2026-05-23 14:32:05 +0700

    chore: add js-cookie + sonner, @/ alias, token env key

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL?: string
-  readonly VITE_USE_MOCKS?: string
+  readonly VITE_API_URL: string
+  readonly VITE_ACCESS_TOKEN_KEY: string
 }
 
 interface ImportMeta {
